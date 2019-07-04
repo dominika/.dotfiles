dd69f827f7782e41b1b101581c04937cdda49ade
dominika <dominika.stempniewicz@gmail.com>
2019-07-04 11:58:54 +0200

    cleanup various changs – change cycle panes bind key [tmux] – add automatic window renumbering [tmux] - use modified gray theme [tmux] – remove ack plugin, add plantuml-syntax [vimrc] - preetify init.fish, add used ENV variables, clean abbrvs, add two update functions [fish]

--- init.fish
+++ init.fish
@@ -1,12 +1,35 @@
-# environment variables
+# +-----------------------------------------------------------------------------+
+# |                                                                             |
+# +-----------------------------------------------------------------------------+
+
+# +-----------------------------------------------------------------------------+
+# | Environment variables                                                       |
+# +-----------------------------------------------------------------------------+
+# +---- rails
 set -xg RAILS_ENV development
 set -xg CHRUBY_ROOT /usr/local/opt/chruby
+# +---- vim plugin for ctags
+set -xg DEP_PROJECT_REG '[admin|engine]'
+set -xg DEP_PROJECT ~/bgl/gaming_worker
+# +---- machine configuration script
+set -xg DOT_FILES ~/.dotfiles
+set -xg BREW_FILES_PATH ~/.brewFiles
+# +---- utility
+set -xg MANPAGER 'col -bx | vim -c ":set ft=man nonu nolist" -R -'
+set -xg EDITOR vim
+set -xg LANG en_US.UTF-8
+
+set -xg PATH $PATH /Users/deszczowiec/Library/Python/3.6/bin
+
+if [ -f '/Users/deszczowiec/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/deszczowiec/Downloads/google-cloud-sdk/path.fish.inc'; end
 
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
-# prompt settings
+# +-----------------------------------------------------------------------------+
+# | Prompt settings                                                             |
+# +-----------------------------------------------------------------------------+
 set -g theme_display_hg no
 set -g theme_display_ruby yes
 set -g theme_display_user no
@@ -20,14 +43,17 @@ set -g fish_prompt_pwd_dir_length 1
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor yes
 
-# abbreviations / aliases
+# +-----------------------------------------------------------------------------+
+# | Abbreviations and aliases                                                   |
+# +-----------------------------------------------------------------------------+
 if not set -q abbrs_initialized
   set -U abbrs_initialized
   echo -n Setting abbreviations...
   abbr b "bundle exec"
   abbr bi 'bundle install'
+  abbr r "bundle exec rails"
+  abbr s "bundle exec rails s puma"
   abbr c "bundle exec rails c 2>/dev/null"
-  abbr ap "git pull and bi and mdb"
   abbr gs "git status"
   abbr gp "git pull"
   abbr gd "git diff"
@@ -36,23 +62,49 @@ if not set -q abbrs_initialized
   abbr gap "git add -p"
   abbr gl "git log"
   abbr gcm "git checkout master"
+  abbr gcb "git checkout -b"
   abbr gmm "git merge master"
   abbr gprune "git remote prune origin"
   abbr gplo "git pull origin (git_branch_name)"
-  abbr gpuo "git push origin (git_branch_name)"
+  abbr gpso "git push origin (git_branch_name)"
   abbr gst "git stash"
   abbr jb jobs
   abbr l 'ls -hF'
   abbr la 'ls -AhF'
   abbr ll 'ls -lhF'
   abbr lla 'ls -lAhF'
-  abbr mdb "bundle exec rake db:migrate and RAILS_ENV=test bundle exec rake db:migrate"
+  abbr mdb "bundle exec rake db:migrate and env RAILS_ENV=test bundle exec rake db:migrate"
   abbr pry "bundle exec pry-remote"
   abbr byebug "byebug -R localhost:8989"
   abbr q exit
-  abbr r "bundle exec rails"
-  abbr s "bundle exec rails s puma"
-  abbr rk "bundle exec rake"
-  abbr rr "bundle exec rake routes"
   abbr v vim
+end
+
+# +-----------------------------------------------------------------------------+
+# | Functions                                                                   |
+# +-----------------------------------------------------------------------------+
+function break_things_for_me --description 'updates all the (system) things: gems, packages, vim plugins'
+  echo (set_color blue) 'Updating omf  ------------------------------------------------------' (set_color normal)
+  omf update
+  echo (set_color blue) 'Updating rubygems  -------------------------------------------------' (set_color normal)
+  gem update --system
+  chruby 2.5.1
+  echo (set_color blue) 'Updating bundler   -------------------------------------------------' (set_color normal)
+  gem install bundler --no-document
+  #gem cleanup -V
+  echo (set_color blue) 'Updating homebrew packages -----------------------------------------'  (set_color normal)
+  cd $DOT_FILES
+  ls -d1 $BREW_FILES_PATH/* | xargs -n1 bash -c 'brew bundle --file=$0'
+  brew doctor
+  echo (set_color blue) 'Upgrading vim & plugins --------------------------------------------' (set_color normal)
+  vim +PlugUpgrade +PlugUpdate +qall
+end
+
+function latest_and_set_up --description 'updates all the (project) things'
+  git stash
+  git pull
+  dotenv2 .env
+  bundle install
+  generate_ctags (basename (pwd)) with_gems
+  bundle exec rake db:migrate && env RAILS_ENV=test bundle exec rake db:migrate
 end
