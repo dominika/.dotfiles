d8051345efe5a3fbda6a799469422b1bcf33de09
dominika <dominika.stempniewicz@gmail.com>
2020-04-05 18:23:18 +0200

    update fish configuration – rename, since currently the docs mention this as a proper config place & name, – remove rails and ruby related lines, – remove python related lines, – remove gsdk related lines, – remove personal script related lines, – tweak the theme.

--- config.fish
+++ config.fish
@@ -5,43 +5,28 @@
 # +-----------------------------------------------------------------------------+
 # | Environment variables                                                       |
 # +-----------------------------------------------------------------------------+
-# +---- rails
-set -xg RAILS_ENV development
-set -xg CHRUBY_ROOT /usr/local/opt/chruby
-# +---- vim plugin for ctags
-set -xg DEP_PROJECT_REG '[admin|engine]'
-set -xg DEP_PROJECT ~/bgl/gaming_worker
-# +---- machine configuration script
-set -xg DOT_FILES ~/.dotfiles
-set -xg BREW_FILES_PATH ~/.brewFiles
 # +---- utility
 set -xg MANPAGER 'col -bx | vim -c ":set ft=man nonu nolist" -R -'
 set -xg EDITOR vim
 set -xg LANG en_US.UTF-8
 
-set -xg PATH $PATH /Users/deszczowiec/Library/Python/3.6/bin
-
-if [ -f '/Users/deszczowiec/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/deszczowiec/Downloads/google-cloud-sdk/path.fish.inc'; end
-
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 # +-----------------------------------------------------------------------------+
 # | Prompt settings                                                             |
 # +-----------------------------------------------------------------------------+
 set -g theme_display_hg no
-set -g theme_display_ruby yes
+set -g theme_display_ruby no
 set -g theme_display_user no
 set -g theme_display_vi no
 set -g theme_display_date no
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts no
 set -g theme_show_exit_status yes
-set -g theme_color_scheme solarized
+set -g theme_color_scheme terminal
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_project_dir_length 1
-set -g theme_newline_cursor yes
+set -g theme_newline_cursor no
 set -U fish_color_autosuggestion white
 
 # +-----------------------------------------------------------------------------+
@@ -87,25 +72,11 @@ end
 function break_things_for_me --description 'updates all the (system) things: gems, packages, vim plugins'
   echo (set_color blue) 'Updating omf  ------------------------------------------------------' (set_color normal)
   omf update
-  echo (set_color blue) 'Updating rubygems  -------------------------------------------------' (set_color normal)
-  gem update --system
-  chruby 2.5.1
-  echo (set_color blue) 'Updating bundler   -------------------------------------------------' (set_color normal)
-  gem install bundler --no-document
-  #gem cleanup -V
   echo (set_color blue) 'Updating homebrew packages -----------------------------------------'  (set_color normal)
-  cd $DOT_FILES
-  ls -d1 $BREW_FILES_PATH/* | xargs -n1 bash -c 'brew bundle --file=$0'
+  cd
+  brew bundle
   brew doctor
+  cd -
   echo (set_color blue) 'Upgrading vim & plugins --------------------------------------------' (set_color normal)
   vim +PlugUpgrade +PlugUpdate +qall
-end
-
-function latest_and_set_up --description 'updates all the (project) things'
-  git stash
-  git pull
-  dotenv2 .env
-  bundle install
-  generate_ctags (basename (pwd)) with_gems
-  bundle exec rake db:migrate && env RAILS_ENV=test bundle exec rake db:migrate
 end
